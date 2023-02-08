27c9eebf868c5c0a2437ea340a47be2c287e2c20
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-02-08 23:52:54 +0300

    Create automatedtwitter.py

---
+++ automatedtwitter.py
@@ -0,0 +1,85 @@
+import tweepy
+import time
+
+# Replace these with your own Twitter API credentials
+consumer_key = "YOUR_CONSUMER_KEY"
+consumer_secret = "YOUR_CONSUMER_SECRET"
+access_token = "YOUR_ACCESS_TOKEN"
+access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
+def like_tweets(query, count):
+    # Search for tweets containing the given query and like them
+    tweets = api.search(q=query, count=count)
+    for tweet in tweets:
+        try:
+            api.create_favorite(tweet.id)
+            print(f"Liked tweet: {tweet.text}")
+        except tweepy.TweepError as error:
+            if error.api_code == 139:
+                # Error code 139 means the tweet has already been liked
+                print(f"Tweet already liked: {tweet.text}")
+            else:
+                print(f"Error liking tweet: {error}")
+
+def retweet_tweets(query, count):
+    # Search for tweets containing the given query and retweet them
+    tweets = api.search(q=query, count=count)
+    for tweet in tweets:
+        try:
+            api.retweet(tweet.id)
+            print(f"Retweeted tweet: {tweet.text}")
+        except tweepy.TweepError as error:
+            if error.api_code == 327:
+                # Error code 327 means the tweet has already been retweeted
+                print(f"Tweet already retweeted: {tweet.text}")
+            else:
+                print(f"Error retweeting tweet: {error}")
+
+def post_tweet(status):
+    # Post a new tweet with the given status
+    try:
+        api.update_status(status=status)
+        print(f"Tweeted: {status}")
+    except tweepy.TweepError as error:
+        if error.api_code == 186:
+            # Error code 186 means the tweet is too long
+            print("Error tweeting: Tweet is too long")
+        else:
+            print(f"Error tweeting: {error}")
+
+def follow_users(query, count):
+    # Search for users containing the given query and follow them
+    users = api.search_users(q=query, count=count)
+    for user in users:
+        try:
+            api.create_friendship(user.id)
+            print(f"Followed user: {user.screen_name}")
+        except tweepy.TweepError as error:
+            if error.api_code == 160:
+                # Error code 160 means the user has already been followed
+                print(f"User already followed: {user.screen_name}")
+            else:
+                print(f"Error following user: {error}")
+
+def automate_interactions(query, count):
+      # Run all functions in a loop with the given interval
+    interval = 3600 # 1 hour
+    while True:
+        like_tweets(query, count)
+        retweet_tweets(query, count)
+        post_tweet("Hello, world! This is an automated tweet.")
+        follow_users(query, count)
+        time.sleep(interval)
+
+# Call the automate_interactions function with the desired query and count
+automate_interactions("#socialmedia", 10)
+
+
+
